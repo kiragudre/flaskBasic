cfd8d3165ab4b3412aac1a7ffeb0f1d2fda84fc9
kiragudre <kiragudre@gmail.com>
2026-04-08 16:03:06 +0100

    Progress

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
 
 @app.route('/login')
 def login():
@@ -19,4 +16,3 @@ def register():
 @app.route('/success')
 def success():
     return render_template('success.html')
-    
---
+++ models.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, index=True)
+    password = db.Column(db.String(20), index=True, unique=False)
